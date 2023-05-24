5dbd54ed7d8f009694807d3c747f163537535d4c
Morgan.F <38009924+Shynen@users.noreply.github.com>
2023-05-24 14:18:22 +0200

    add function

--- src/main/js/main.js
+++ src/main/js/main.js
@@ -151,3 +151,4 @@ function checkDoubleOperateur(text, element){
   }
 
   //#endregion
+
--- src/main/js/scientifique.js
+++ src/main/js/scientifique.js
@@ -1,8 +1,32 @@
-function getCos() {
+
+function getCos(input) {
     // let ecran = document.getElementById("display");
     // let resultat = Math.PI += " ";
     // ecran.value += resultat;
-    let input = document.getElementById("cosBtn").value;
-    console.log(input);
+    let result = parseFloat(input);
+    result = eval (Math.cos(input));
+    console.log(input)
+    console.log(result);
+    console.log(eval(input))
+    return result;
 }
+
+function calcul(){
+    let chiffre1 = document.getElementsByClassName("button").value;
+    let chiffre2 = document.getElementsByClassName("button").value;
+    switch(operateur){
+        case '+': return chiffre1 + chiffre2;
+        case '-': return chiffre1 - chiffre2;
+        case '*': return chiffre1 * chiffre2;
+        case '/': return chiffre1 / chiffre2;
+        case 'cos': return Math.cos(chiffre1);
+        //etc ...
+    }
+    console.log( document.getElementsByClassName("button").value)
+}
+// Si tu parles de la touche 10^x, il suffit de remplacer le chiffre affiché par le resultat de (Math.pow(10, parseFloat(document.getElementsByName('box_resultat')[0].value) || 0);
+
+
+ //#region Fonction du Display Text (Écran de la calculatrice)
+
 
