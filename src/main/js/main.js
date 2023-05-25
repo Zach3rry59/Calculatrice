d5e1677f559bbe8394a1b14c10b2938e0684ab01
Zach3rry59 <draks.nathan@gmail.com>
2023-05-25 13:11:47 +0200

    merge javascript modify html

--- src/main/js/main.js
+++ src/main/js/main.js
@@ -254,3 +254,25 @@ function checkDoubleElement(calcul, element){
   }
 
 //#endregion
+
+function swap() {
+  const swapMode = document.getElementById("swapMode");
+  const box = document.getElementById("box");
+  const container = document.getElementById("container");
+  const buttonsSci = document.querySelectorAll('input[swap="sci"]');
+  if (swapMode.className == "buttonsSci") {
+    swapMode.className = "buttons";
+    box.className = "box";
+    container.className = "container";
+    buttonsSci.forEach((button) => {
+      button.style.display = "none";
+    });
+  } else {
+    swapMode.className = "buttonsSci";
+    box.className = "boxSci";
+    container.className = "containerSci";
+    buttonsSci.forEach((button) => {
+      button.style.display = "grid";
+    });
+  }
+}
